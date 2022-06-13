069690f7b56a88725223aa18af8d22885c15bd05
Tóth Gabriella <thgabi1@gmail.com>
2022-06-13 11:25:10 +0200

    setup Flask

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+def main():
+    app.run(debug=True)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    main()
